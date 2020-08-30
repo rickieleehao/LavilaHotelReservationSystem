f14023d82187653302e740efe79b1bc91f5222fe
Koh Jun Dong <KohComputer@gmail.com>
2020-08-31 07:34:10 +0800

    Initial design and event handler software

---
+++ AssignmentFX/src/application/GuestController.java
@@ -0,0 +1,37 @@
+package application;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.MenuButton;
+import javafx.scene.control.TextField;
+
+public class GuestController {
+
+    @FXML
+    private MenuButton stateDropdown;
+
+    @FXML
+    private TextField firstNameText;
+
+    @FXML
+    private TextField lastNameText;
+
+    @FXML
+    private TextField add1Text;
+
+    @FXML
+    private TextField add2Text;
+
+    @FXML
+    private TextField postCodeText;
+
+    @FXML
+    private TextField idText;
+
+    @FXML
+    private Button searchBtn;
+
+    @FXML
+    private Button addGuestBtn;
+
+}
